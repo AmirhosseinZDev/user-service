5334d418b5a4df6cefb2fd754e86c67f7592d865
HamidShahabi <mrhr5310@gmail.com>
2025-04-15 21:51:36 +0330

    improvement(user-service): Use lombok annotations instead of redundant getters and setters

--- src/main/java/com/ftgo/user/userservice/entity/BaseUser.java
+++ src/main/java/com/ftgo/user/userservice/entity/BaseUser.java
@@ -2,12 +2,14 @@ package com.ftgo.user.userservice.entity;
 
 import com.ftgo.user.userservice.entity.converter.GrantedAuthorityConverter;
 import jakarta.persistence.*;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 
+@Data
 @Entity
 public class BaseUser implements Serializable {
     @Serial
@@ -34,43 +36,5 @@ public class BaseUser implements Serializable {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getUsername() {
-        return username;
-    }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public List<GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    public void setAuthorities(List<GrantedAuthority> authorities) {
-        this.authorities = authorities;
-    }
 }
