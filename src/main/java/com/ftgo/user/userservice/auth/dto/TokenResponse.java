28330a4ada7b715829cc6047367b12844d1cfcd7
HamidShahabi <mrhr5310@gmail.com>
2025-04-15 22:23:04 +0330

    improvement(user-service): Use lombok annotations instead of redundant getters and setters

--- src/main/java/com/ftgo/user/userservice/auth/dto/TokenResponse.java
+++ src/main/java/com/ftgo/user/userservice/auth/dto/TokenResponse.java
@@ -1,8 +1,12 @@
 package com.ftgo.user.userservice.auth.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class TokenResponse {
 
     private String token;
