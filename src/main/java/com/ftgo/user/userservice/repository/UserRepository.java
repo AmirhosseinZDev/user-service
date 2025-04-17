7fb1335ecf43e5c73fced58a702026b0efc5e065
HamidShahabi <mrhr5310@gmail.com>
2025-04-17 22:16:20 +0330

    chore(db): Remove unused UserRepository methods
    - Removes findByEmail and findByPhoneNumber methods from UserRepository
      as they were not used.

--- src/main/java/com/ftgo/user/userservice/repository/UserRepository.java
+++ src/main/java/com/ftgo/user/userservice/repository/UserRepository.java
@@ -5,7 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<BaseUser, Long> {
     BaseUser findByUsername(String username);
-    BaseUser findByEmail(String email);
-    BaseUser findByPhoneNumber(String phoneNumber);
-
 }
